e7eb79af97e0d719597a261e1738b6fec7b896d5
Glitch (blaze-haircut) <none>
2018-04-17 18:21:32 +0000

    💆🚀 Updated with Glitch

---
+++ src/components/email.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Sidebar from './sidebar';
+import SingleEmail from './single-email';
+import EmailList from './email-list';
+import './email.css';
+import {Route} from 'react-router-dom';
+
+export default function Email() {
+    return (
+        <div className="email">
+            <Route path='/' component={Sidebar} />
+            <main>
+                <Route path='/:folderId' exact component={EmailList} folderId="inbox" />
+                {/* <SingleEmail folderId="inbox" emailId="1" /> */}
+            </main>
+        </div>
+    );
+}
